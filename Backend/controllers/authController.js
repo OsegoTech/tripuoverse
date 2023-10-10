6e5559fecab74f7eee8e7dd37b2e795f036834a4
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-10-10 13:06:32 +0300

    logging in userson signup using jwt

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,10 +1,25 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
 
 export const signup = asyncHandler(async (req, res) => {
-  const newUser = await User.create(req.body);
+//   const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        whatsApp: req.body.whatsApp,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
