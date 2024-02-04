929c1cee33526d349d7b610d6181f11ea97ce760
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2024-02-04 13:42:43 +0300

    create product form

--- src/components/Dashboard/UserDashboard.vue
+++ src/components/Dashboard/UserDashboard.vue
@@ -8,11 +8,22 @@
           >, Welcome to your Dashbord
         </h1>
         <div class="flex gap-1">
-          <button class="bg-blue-500 text-white py-2 px-3 rounded-md">
+          <button
+            data-modal-target="create-product-modal"
+            data-modal-toggle="create-product-modal"
+            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+          >
             New Product
           </button>
-          <button class="bg-blue-500 text-white py-2 px-3 rounded-md">
-            New Listing
+          <!-- Modal toggle -->
+          <button
+            data-modal-target="crud-modal"
+            data-modal-toggle="crud-modal"
+            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+          >
+            New Service
           </button>
         </div>
       </div>
@@ -107,16 +118,29 @@
       </div>
     </div>
     <CreateProduct />
+    <CreateService />
     <TheFooter />
   </div>
 </template>
 
 <script setup>
 import CreateProduct from "../CreateProduct.vue";
+import CreateService from "../CreateService.vue";
 import TheFooter from "../TheFooter.vue";
 import TheNavigate from "../TheNavigate.vue";
+import { onMounted, ref } from "vue";
+import { initModals } from "flowbite";
 
 const sellername = "Jeff";
+onMounted(() => {
+  initModals();
+});
+
+const showCreateService = ref(false);
+
+const toggleCreateService = () => {
+  showCreateService.value = true;
+};
 </script>
 
 <style lang="scss" scoped></style>
