db200d51753842524efe5004ddd9eb9edae2c1cc
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2024-02-12 11:56:05 +0300

    Added test credentials

--- src/Views/LoginView.vue
+++ src/Views/LoginView.vue
@@ -41,7 +41,7 @@
                   name="email"
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="superadmin@gmail.com"
                   required=""
                 />
               </div>
@@ -56,7 +56,7 @@
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="••••••••"
+                  placeholder="test1234"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
